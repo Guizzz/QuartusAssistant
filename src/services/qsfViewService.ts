30862af34ee71f737a71a08a1d407bb96ebb17a5
Andrea Guizzaro <45075994+Guizzz@users.noreply.github.com>
2026-05-27 15:08:51 +0200

    auto reload tree view after .do generation

--- src/services/qsfViewService.ts
+++ src/services/qsfViewService.ts
@@ -11,12 +11,33 @@ export async function registerQsfView(context: vscode.ExtensionContext)
             'quartus-assistant-view',
             tabView
         );
+    
+    const watcher = vscode.workspace.createFileSystemWatcher('**/*.{qsf,do}');
+    watcher.onDidCreate(
+        async () => {
+            await tabView.loadData();
+        }
+    );
+    
+    const deleteListener =
+        vscode.workspace.onDidDeleteFiles(
+            async (docs: vscode.FileDeleteEvent) => 
+            {   
+                for (const d of docs.files)
+                {
+                    if (d.path.endsWith('.qsf') || d.path.endsWith('.do'))
+                    {
+                       await tabView.loadData(); 
+                    }
+                }
+            }
+        );
 
     const saveListener =
         vscode.workspace.onDidSaveTextDocument(
             async (doc) => 
             {
-                if (doc.fileName.endsWith('.qsf')) 
+                if (doc.fileName.endsWith('.qsf') || doc.fileName.endsWith('.do')) 
                 {
                     await tabView.loadData();
                 }
@@ -25,6 +46,7 @@ export async function registerQsfView(context: vscode.ExtensionContext)
 
     context.subscriptions.push(
         treeProvider,
-        saveListener
+        saveListener,
+        deleteListener
     );
 }
