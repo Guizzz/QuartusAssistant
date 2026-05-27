b36d53b283662582de8289adc364c452fbaeb63b
Andrea Guizzaro <45075994+Guizzz@users.noreply.github.com>
2026-05-27 15:23:29 +0200

    auto reload when new test bench is created. Fixes #2

--- src/services/qsfViewService.ts
+++ src/services/qsfViewService.ts
@@ -12,20 +12,34 @@ export async function registerQsfView(context: vscode.ExtensionContext)
             tabView
         );
     
-    const watcher = vscode.workspace.createFileSystemWatcher('**/*.{qsf,do}');
+    const watcher = vscode.workspace.createFileSystemWatcher('**/*.{qsf,do,vhd}');
     watcher.onDidCreate(
         async () => {
             await tabView.loadData();
         }
     );
     
+    const createListener =
+        vscode.workspace.onDidCreateFiles(
+            async (docs: vscode.FileCreateEvent) => 
+            {   
+                for (const d of docs.files)
+                {   
+                    if (d.path.endsWith('.qsf') || d.path.endsWith('.do') || d.path.endsWith('.vhd'))
+                    {
+                       await tabView.loadData(); 
+                    }
+                }
+            }
+        );
+
     const deleteListener =
         vscode.workspace.onDidDeleteFiles(
             async (docs: vscode.FileDeleteEvent) => 
             {   
                 for (const d of docs.files)
                 {
-                    if (d.path.endsWith('.qsf') || d.path.endsWith('.do'))
+                    if (d.path.endsWith('.qsf') || d.path.endsWith('.do') || d.path.endsWith('.vhd'))
                     {
                        await tabView.loadData(); 
                     }
@@ -37,7 +51,7 @@ export async function registerQsfView(context: vscode.ExtensionContext)
         vscode.workspace.onDidSaveTextDocument(
             async (doc) => 
             {
-                if (doc.fileName.endsWith('.qsf') || doc.fileName.endsWith('.do')) 
+                if (doc.fileName.endsWith('.qsf') || doc.fileName.endsWith('.do') || doc.fileName.endsWith('.vhd')) 
                 {
                     await tabView.loadData();
                 }
@@ -47,6 +61,7 @@ export async function registerQsfView(context: vscode.ExtensionContext)
     context.subscriptions.push(
         treeProvider,
         saveListener,
+        createListener,
         deleteListener
     );
 }
